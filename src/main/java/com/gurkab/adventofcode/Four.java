60b62a4fe69509ad6bdc8076f80cb6bdbf696fe0
gurka <gurkab@gmail.com>
2020-12-15 08:39:02 -0800

    Part 1 for day 4

--- src/main/java/com/gurkab/adventofcode/Four.java
+++ src/main/java/com/gurkab/adventofcode/Four.java
@@ -9,31 +9,100 @@ import java.util.Scanner;
 public class Four {
 
     public Integer solvePartOne() throws FileNotFoundException {
-        final List<String> input = new ArrayList<String>();
+        int answer = 0;
+        List<String> fileInput = new ArrayList<String>();
+        List<List<String>> input = new ArrayList<>();
         File file = new File("src/main/resources/4.input");
         Scanner scanner = new Scanner(file);
-        int index = 0;
         while (scanner.hasNext()) {
-            if (scanner.nextLine().length() == 0) {
-                System.out.println("length 0!");
+            fileInput.add(scanner.nextLine());
+        }
+        int index = 0;
+        for (String line : fileInput) {
+            if (line.length() == 0) {
                 index++;
                 continue;
             }
-            String existingString = "";
-            try {
-                existingString = input.get(index);
-            } catch (Exception ignored) {}
-            System.out.println(existingString);
-            System.out.println(" ");
-            System.out.println(scanner.nextLine());
-            input.add(new StringBuilder().append(existingString).append(" ").append(scanner.nextLine()).toString());
-            index++;
-            System.out.println("made it to end index is " + index);
+            for (String field : line.split(" ")) {
+                try {
+                    input.get(index).add(field);
+                } catch (IndexOutOfBoundsException ignored) {
+                    input.add(new ArrayList<>());
+                    input.get(index).add(field);
+                }
+            }
         }
 
-        System.out.println(input);
+        for (List<String> list : input) {
+            String evaluate = "";
+            for (String field : list) {
+                evaluate = new StringBuilder().append(evaluate).append(" ").append(field).toString();
+            }
+            if (evaluate.contains("byr:") && evaluate.contains("iyr:") && evaluate.contains("eyr:") && evaluate.contains("hgt:") && evaluate.contains("hcl:") && evaluate.contains("ecl:") && evaluate.contains("pid:")) {
+                answer ++;
+            }
+        }
+
+        return answer;
+    }
+
+    public Integer solvePartTwo() throws FileNotFoundException {
+        int answer = 0;
+        List<String> fileInput = new ArrayList<String>();
+        List<List<String>> input = new ArrayList<>();
+        File file = new File("src/main/resources/4.input");
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNext()) {
+            fileInput.add(scanner.nextLine());
+        }
+        int index = 0;
+        for (String line : fileInput) {
+            if (line.length() == 0) {
+                index++;
+                continue;
+            }
+            for (String field : line.split(" ")) {
+                try {
+                    input.get(index).add(field);
+                } catch (IndexOutOfBoundsException ignored) {
+                    input.add(new ArrayList<>());
+                    input.get(index).add(field);
+                }
+            }
+        }
+
+        mainLoop: for (List<String> list : input) {
+            String evaluate = "";
+            for (String field : list) {
+                evaluate = new StringBuilder().append(evaluate).append(" ").append(field).toString();
+            }
+            if (evaluate.contains("byr:") && evaluate.contains("iyr:") && evaluate.contains("eyr:") && evaluate.contains("hgt:") && evaluate.contains("hcl:") && evaluate.contains("ecl:") && evaluate.contains("pid:")) {
+                for (String field : list) {
+                    String fieldType = field.split(":")[0];
+                    String fieldValue = field.split(":")[1];
+                    switch (fieldType) {
+                        case "byr":
+                            if (fieldValue.length() == 4 && (Integer.parseInt(fieldValue) >= 1920 && Integer.parseInt(fieldValue) <= 2002)) {
+                                break;
+                            }
+                            continue mainLoop;
+                        case "iyr":
+                            if (fieldValue.length() == 4 && (Integer.parseInt(fieldValue) >= 2010 && Integer.parseInt(fieldValue) <= 2002)) {
+                                break;
+                            }
+                            continue mainLoop;
+                        case "eyr":
+                            if (fieldValue.length() == 4 && (Integer.parseInt(fieldValue) >= 2020 && Integer.parseInt(fieldValue) <= 2030)) {
+                                break;
+                            }
+                            continue mainLoop;
+                    }
+                }
+            }
+        }
+
+
 
         return null;
-
     }
 }
